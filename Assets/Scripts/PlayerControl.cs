9615d12d78fcc9f7329d213140ef7cf1dfb9e822
Chema Leon <chema@playbiba.com>
2016-01-29 21:56:42 -0800

    Started prototype setup

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public int PlayerNumber = 1;
+
+	private Rigidbody _rigidbody;
+
+	void Awake() {
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		_rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal_1"), 0f, Input.GetAxis("Vertical_1"));
+	}
+}

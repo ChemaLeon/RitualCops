f15b48df87d1ad2fa2b0b4fc2a65b5ed64351b93
Chema Leon <chema@playbiba.com>
2016-01-31 05:30:25 -0800

    Physics to the rag dolls!

---
+++ Assets/Scripts/RagdollPhysics.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RagdollPhysics : MonoBehaviour {
+
+	public Rigidbody spineRigidbody;
+
+	void Start () {
+		spineRigidbody.AddForce((-transform.forward+transform.up)*Random.Range(300f, 600f));
+	}
+
+}
